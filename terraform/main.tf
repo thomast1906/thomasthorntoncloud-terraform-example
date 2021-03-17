56e838011de74553a93a2c95ffcc8031b78b788f
Thomas Thornton <>
2021-03-17 18:13:49 +0000

    update fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x.
-    # If you're using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
-    features {}
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
 }
 
 data "azurerm_client_config" "current" {}
@@ -15,17 +15,17 @@ resource "azurerm_resource_group" "tamops" {
 
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-    name                = "tamops-vnet"
-    address_space       = ["192.168.0.0/16"]
-    location            = "eastus2"
-    resource_group_name = azurerm_resource_group.tamops.name
+  name                = "tamops-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.tamops.name
 }
 
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
-  name                      = "subnet"
-  resource_group_name       = azurerm_resource_group.tamops.name
-  virtual_network_name      = azurerm_virtual_network.vnet.name
-  address_prefix            = "192.168.0.0/24"
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.tamops.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
 }
   
