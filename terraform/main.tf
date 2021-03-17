72487a004b2073badac33a27b4fb254e42787ddb
Thomas Thornton <>
2021-03-17 19:34:19 +0000

    no message

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tamopstfstates"
+    storage_account_name = "tfstatedevops"
+    container_name       = "terraformgithubexample"
+    key                  = "terraformgithubexample.tfstate"
+  }
+}
+
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
